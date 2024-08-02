dd5ad57064a449cd3c5712553bbcedd48d577d8e
Satsuki Akiba <anosatsuk124@gmail.com>
2024-08-02 19:45:25 +0900

    Add a support for new-class literals

--- Demo/SourcesGenerated/MyMain.generated.cs
+++ Demo/SourcesGenerated/MyMain.generated.cs
@@ -6,6 +6,7 @@ public override void _Ready()
 var root_0 = this;
 var scene_0 = GD.Load<PackedScene>("res://main.tscn");
 var node_0 = scene_0.Instantiate<Main>();
+node_0.Position = new Vector2(213, 11);
 root_0.AddChild(node_0);
 var root_1 = node_0;
 }
--- Sources/Literal.cs
+++ Sources/Literal.cs
@@ -4,50 +4,73 @@ namespace GodotUIXml;
 
 public abstract record Literal()
 {
-    public sealed record NumberLiteral(double Value) : Literal();
+    public abstract string ToCSharpLiteral();
 
-    public sealed record StringLiteral(string Value) : Literal();
+    public sealed record NumberLiteral(double Value) : Literal()
+    {
+        public override string ToCSharpLiteral()
+    => Value.ToString();
+    }
 
-    public sealed record BooleanLiteral(bool Value) : Literal();
+    public sealed record StringLiteral(string Value) : Literal()
+    {
+        public override string ToCSharpLiteral() =>
+            $"\"{Value}\"";
+    }
 
-    public sealed record ArrayLiteral(List<Literal> Value) : Literal();
+    public sealed record BooleanLiteral(bool Value) : Literal()
+    {
+        public override string ToCSharpLiteral()
+            => Value.ToString().ToLower();
+    }
+
+    public sealed record ListLiteral(List<Literal> Value) : Literal()
+    {
+        public override string ToCSharpLiteral()
+        => string.Join(", ", Value.Select(x => x.ToCSharpLiteral()));
+    }
+
+    public sealed record ArrayLiteral(ListLiteral Value) : Literal()
+    {
+        public override string ToCSharpLiteral()
+            => $"[ {Value.ToCSharpLiteral()} ]";
+    }
+
+    public sealed record ClassLiteral(string Name, ListLiteral Value) : Literal()
+    {
+        public override string ToCSharpLiteral()
+            => $"new {Name}({Value.ToCSharpLiteral()})";
+    }
 
     public abstract record VectorLiteral() : Literal();
 
-    public sealed record Vector2Literal(NumberLiteral X, NumberLiteral Y) : VectorLiteral();
-    public sealed record Vector3Literal(NumberLiteral X, NumberLiteral Y, NumberLiteral Z) : VectorLiteral();
+    public sealed record Vector2Literal(NumberLiteral X, NumberLiteral Y) : VectorLiteral()
+    {
+        public override string ToCSharpLiteral()
+            => $"new Vector2({X.ToCSharpLiteral()}, {Y.ToCSharpLiteral()})";
+    }
+
+    public sealed record Vector3Literal(NumberLiteral X, NumberLiteral Y, NumberLiteral Z) : VectorLiteral()
+    {
+        public override string ToCSharpLiteral()
+            => $"new Vector3({X.ToCSharpLiteral()}, {Y.ToCSharpLiteral()}, {Z.ToCSharpLiteral()})";
+    }
 
     public static Literal Parse(string text)
     {
         return text switch
         {
-            ['#', .. var s] => ParseNumber(s),
-            ['"', .. var s, '"'] => ParseString(s),
-            ['\'', .. var s, '\''] => ParseString(s),
-            ['[', .. var s, ']'] => ParseArray(s),
-            ['(', .. var s, ')'] => ParseVector(s),
+        ['#', .. var s] => ParseNumber(s),
+        ['@', .. var s] => ParseClass(s),
+        ['"', .. var s, '"'] => ParseString(s),
+        ['\'', .. var s, '\''] => ParseString(s),
+        ['[', .. var s, ']'] => ParseArray(s),
+        ['(', .. var s, ')'] => ParseVector(s),
             "true" or "false" => ParseBoolean(text),
             _ => ParseString(text),
         };
     }
 
-    public string ToCSharpLiteral()
-    {
-        return this switch
-        {
-            NumberLiteral n => n.Value.ToString(),
-            StringLiteral s => $"\"{s.Value.ToString()}\"",
-            BooleanLiteral b => b.Value.ToString().ToLower(),
-            ArrayLiteral a => $"[ {string.Join(", ", a.Value.Select(x => x.ToCSharpLiteral()))} ]",
-            VectorLiteral v => v switch
-            {
-                Vector2Literal v2 => $"new Vector2({v2.X.ToCSharpLiteral()}, {v2.Y.ToCSharpLiteral()})",
-                Vector3Literal v3 => $"new Vector3({v3.X.ToCSharpLiteral()}, {v3.Y.ToCSharpLiteral()}, {v3.Z.ToCSharpLiteral()})",
-                _ => throw new NotImplementedException()
-            },
-            _ => throw new NotImplementedException()
-        };
-    }
 
     protected static NumberLiteral ParseNumber(string input)
     {
@@ -67,13 +90,33 @@ public abstract record Literal()
 
     protected static ArrayLiteral ParseArray(string input)
     {
+        var list = ParseList(input);
+        return new ArrayLiteral(list);
+    }
+
+    protected static ClassLiteral ParseClass(string input, string? name = null)
+    {
+        if (name is not null)
+        {
+            var listString = input.Substring(name.Length).TrimStart('(').TrimEnd(')');
+            return new ClassLiteral(name, ParseList(listString));
+        }
+
+        var _name = input.Split('(').First();
+
+        return ParseClass(input, _name);
+    }
+
+    private static ListLiteral ParseList(string input)
+    {
         var array = new List<Literal>();
         var splited = input.Split(',').Select(x => x.Trim());
         foreach (var item in splited)
         {
             array.Add(Parse(item));
         }
-        return new ArrayLiteral(array);
+
+        return new ListLiteral(array);
     }
 
     protected static VectorLiteral ParseVector(string input)
